a3c04b2365af25d30830b2148020612d19b6a8c9
Zelalem Mekonnen <mekonnen.zelalem251@gmail.com>
2024-09-03 15:51:18 +0300

    Refactor and restyle

--- src/components/simple-component/SimpleComponent.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/SimpleComponent.tsx
-import React from 'react';
-
-const SimpleComponent: React.FC = () => {
-  return <div>Hello, World!</div>;
-};
-
-export default SimpleComponent;
